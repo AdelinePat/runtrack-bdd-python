13c4ac88b7d765b59c20ec26ba20f067991974ba
Adeline <adeline.patenne@hotmail.fr>
2025-03-11 16:15:27 +0100

    job 7 done, using only python script and add commit method

--- day2/job7.py
+++ day2/job7.py
@@ -1,24 +1,84 @@
 import mysql.connector
 
-my_server = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=""
-)
+class Employees():
+    def __init__(self, employee_database, cursor):
+        self.cursor = cursor
+        self.employee_database = employee_database
 
-if my_server.is_connected():
-    cursor = my_server.cursor()
-    cursor.execute("SHOW DATABASES;")
-    databases = cursor.fetchall()
+    # Create
+    def add_employee(self, lastname, firstname, salary, id_service):
+        self.cursor.execute(f"INSERT INTO employee (lastname, firstname, salary, id_service) VALUES ('{lastname}', '{firstname}', {salary}, {id_service});")
+        self.employee_database.commit()
+    # Read
+    def display_employee(self, lastname, firstname):
+        self.cursor.execute(f"SELECT * FROM employee WHERE lastname = '{lastname}' AND firstname = '{firstname}';")
+        return self.cursor.fetchall()
+    
+    def display_all_employee(self):
+        self.cursor.execute(f"TABLE employee")
+        employees = self.cursor.fetchall()
 
-    all_data_bases = []
-    for database in databases:
-        all_data_bases.append(database[0])
+        for employee in employees:
+            print(f"{employee[0]} | {employee[1]} {employee[2]} | {employee[3]} | {employee[4]}")
 
-    if "employees_base" not in all_data_bases:
-        cursor.execute("CREATE DATABASE employees_base;")
+    # Update
+    def update_info(self, info_to_change, new_value, lastname, firstname):
+        if info_to_change == "salary":
+            self.cursor.execute(f"UPDATE employee SET salary = {new_value} WHERE lastname = '{lastname}' and firstname = '{firstname}';")
+        elif info_to_change == "id_service":
+            self.cursor.execute(f"UPDATE employee SET id_service = {new_value} WHERE lastname = '{lastname}' and firstname = '{firstname}';")
+        elif info_to_change == "lastname":
+            self.cursor.execute(f"UPDATE employee SET lastname = '{new_value}' WHERE lastname = '{lastname}' and firstname = '{firstname}';")
+        elif info_to_change == "firstname":
+            self.cursor.execute(f"UPDATE employee SET firstname = '{new_value}' WHERE lastname = '{lastname}' and firstname = '{firstname}';")
+        self.employee_database.commit()
+    # Delete
+    def delete_employee(self, lastname, firstname):
+        self.cursor.execute(f"SELECT * FROM employee WHERE lastname = '{lastname}' and firstname = '{firstname}';")
+        employee = self.cursor.fetchall()
+        if not bool(employee):
+            print("Vous ne pouvez pas supprimer un(e) employé(e) inexistant(e).")
+        else:
+            info = self.display_employee(lastname, firstname)
+            self.cursor.execute(f"DELETE FROM employee WHERE lastname = '{lastname}' AND firstname = '{firstname}';")
+            self.employee_database.commit()
+            print(f"{info}\
+                \nl'employé(e) {lastname} {firstname} a bien été supprimé(e) de la base de données")
 
-    else:
+class DatabaseEmployee():
+    def connect_to_database(self, database_name):
+        database_connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database = database_name
+        )
+        return database_connection
+        
+
+    def create_database(self):
+        my_server = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=""
+        )
+
+        if my_server.is_connected():
+            cursor = my_server.cursor()
+            cursor.execute("SHOW DATABASES;")
+            databases = cursor.fetchall()
+
+            all_data_bases = []
+            for database in databases:
+                all_data_bases.append(database[0])
+
+            if "employees_base" not in all_data_bases:
+                cursor.execute("CREATE DATABASE employees_base;")
+
+            cursor.close()
+        my_server.close()
+
+    def create_tables(self, cursor):
         cursor.execute("USE employees_base;")
         cursor.execute("SHOW TABLES;")
         tables = cursor.fetchall()
@@ -26,37 +86,61 @@ if my_server.is_connected():
         all_tables = []
         for table in tables:
             all_tables.append(table[0])
+
         if "employee" not in all_tables:
-            cursor.execute("USE employees_base; CREATE TABLE employee (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, lastname VARCHAR (255) NOT NULL, firstname VARCHAR(255) NOT NULL, salary FLOAT NOT NULL, id_service INT);")
+            cursor.execute("CREATE TABLE employee (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, lastname VARCHAR (255) NOT NULL, firstname VARCHAR(255) NOT NULL, salary FLOAT NOT NULL, id_service INT);")
         if "service" not in all_tables:
-            cursor.execute("USE employees_base; CREATE TABLE service (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, service_name VARCHAR (255) NOT NULL);")
-    cursor.close()
-my_server.close()
+            cursor.execute("CREATE TABLE service (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, service_name VARCHAR (255) NOT NULL);")
 
-employee_database = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database = "employees_base"
-)
+    def is_table_full(self, cursor, table_name):
+        match table_name:
+            case "service":
+                cursor.execute("TABLE service;")
+            case "employee":
+                cursor.execute("TABLE employee;")
 
-if employee_database.is_connected():
-    cursor = employee_database.cursor()
-    cursor.execute("TABLE service;")
-    is_service = cursor.fetchall()
+        is_empty = cursor.fetchall()
+        if is_empty == []:
+            return False
+        else:
+            return True
+        # return bool(is_empty)
 
-    if not is_service:
-        cursor.execute("INSERT INTO service (service_name) VALUES ('Logistique'),('Ressource Humaine'), ('Informatique'), ('Marketing'), ('Finance');")
+def main():
+    my_database = DatabaseEmployee()
+    my_database.create_database()
+    employee_database = my_database.connect_to_database("employees_base")
+    if employee_database.is_connected():
+        cursor = employee_database.cursor()
+        my_database.create_tables(cursor)
+        my_employees = Employees(employee_database, cursor)
 
-    cursor.execute("TABLE employee")
-    is_employee = cursor.fetchall()
-    if not is_employee:
-        cursor.execute("INSERT INTO employee (lastname, firstname, salary, id_service) VALUES ('Dupont', 'Marie', 3200.0, 1), ('Lefevre', 'Julien', 2800.50, 2), ('Bernard', 'Sophie', 3500.75, 3), ('Moreau', 'Thomas', 4100.80, 4), ('Lambert', 'Claire', 2600, 0);")
+        if not my_database.is_table_full(cursor, 'service'):
+            cursor.execute("INSERT INTO service (service_name) VALUES ('Logistique'),('Ressource Humaine'), ('Informatique'), ('Marketing'), ('Finance');")
+        
+        if not my_database.is_table_full(cursor, 'employee'):
+            my_employees.add_employee('Dupont', 'Marie', 3200.0, 1)
+            my_employees.add_employee('Lefevre', 'Julien', 2800.50, 2)
+            my_employees.add_employee('Bernard', 'Sophie', 3500.75, 3)
+            my_employees.add_employee('Moreau', 'Thomas', 4100.80, 4)
+            my_employees.add_employee('Lambert', 'Claire', 2600, 0)
+            my_employees.add_employee('Martin', 'Lucas', 2900.5, 1)
+       
+        cursor.execute("SELECT * FROM employee WHERE salary > 3000;")
+        hight_salary = cursor.fetchall()
+        print(hight_salary)
 
-    cursor.execute("SELECT * FROM employee WHERE salary > 3000;")
-    hight_salary = cursor.fetchall()
-    print(hight_salary)
+        print(" ### ### ###")
+        my_employees.display_all_employee()
 
-    cursor.close()
+        print(my_employees.display_employee('Martin', 'Lucas'))
 
-employee_database.close()
+        my_employees.delete_employee('Bernard', 'Sophie')
+
+        print(my_employees.display_all_employee())
+
+        cursor.close()
+
+    employee_database.close()
+
+main()
